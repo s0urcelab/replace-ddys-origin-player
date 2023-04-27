8d730f8b55c833b1d7829e7f649c9765434ca31e
冇 <me@src.moe>
2023-04-27 14:48:30 +0800

    optimize region select

--- script.user.js
+++ script.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Replace ddys Origin Player
 // @namespace    https://github.com/s0urcelab/replace-ddys-origin-player
-// @version      1.1
-// @description  替换ddys原始播放器，移除去广告限制，修复滚轮和全屏快捷键失效bug，优化选集功能，自动记忆选集和上次播放位置
+// @version      1.2
+// @description  替换ddys播放器，移除Adblock屏蔽，修复滚轮和全屏快捷键失效bug，优化选集和线路功能，自动记忆选集
 // @author       s0urce
 // @match        https://ddys.art/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ddys.art
@@ -25,7 +25,10 @@ const globalStyle = `
     border: none!important;
     background: none!important;
 }
-.tabs-root {
+.entry > p {
+    display: none;
+}
+.player-sider {
     width: 220px;
     display: flex;
     flex-direction: column;
@@ -56,8 +59,54 @@ const globalStyle = `
     font-size: 14px;
     margin-right: 5px;
 }
+.switch-root {
+    cursor: pointer;
+    user-select: none;
+    position: relative;
+    background: #e0e0e0;
+    border-radius: 26px;
+    width: 174px;
+    padding: 2px;
+}
+.sw-group {
+    position: absolute;
+    top: 0;
+    left: 0;
+    padding: 2px;
+    width: 170px;
+    display: flex;
+    justify-content: space-between;
+}
+.sw-item {
+    line-height: 30px;
+    padding: 0 10px;
+    color: #666;
+}
+.sw-item.active {
+    font-weight: bold;
+    color: #333;
+}
+.switch-root > .indicator {
+    transition: all 0.2s ease;
+    margin-left: 0;
+    width: 86px;
+    height: 30px;
+    background: #fff;
+    border-radius: 26px;
+    box-shadow: 0px 0px 6px -2px #111;
+}
+.switch-root > .indicator.overseas {
+    margin-left: 84px;
+}
+.ep-tip {
+    margin: 10px 0;
+    color: white;
+}
 `
-function parseResUrl(d) {
+function parseResUrl(region, d) {
+    // 海外线路
+    if (region == 'overseas') return { ...d, url: `https://w.ddys.art${d.src0}?ddrkey=${d.src2}` }
+
     const domain = window.location.hostname
     return new Promise((resolve, reject) => {
         GM_xmlhttpRequest({
@@ -66,7 +115,7 @@ function parseResUrl(d) {
             headers: {
                 'referer': `https://${domain}/`
             },
-            url: `https://${domain}/getvddr/video?id=${d.url}&dim=1080P&type=mix`,
+            url: `https://${domain}/getvddr/video?id=${d.src1}&dim=1080P&type=mix`,
             onload: res => {
                 resolve({ ...d, url: res.response.url })
             },
@@ -110,6 +159,39 @@ class Tabs {
         }
     }
 }
+class Switch {
+    constructor(init) {
+        this.root = init.root
+        this.data = init.data
+        this.onSelect = init.onSwitch
+        this.selectedKey = init.data[0].key
+    }
+
+    render(key = this.selectedKey) {
+        // update selectedKey
+        this.selectedKey = key
+        // render dom
+        const group = this.data.reduce((acc, curr) => {
+            const isTarget = key === curr.key
+            return `${acc}
+                <div class="sw-item ${isTarget ? 'active' : ''}" data-sw-key="${curr.key}">
+                ${curr.label}
+            </div>
+            `
+        }, '')
+        this.root.innerHTML = `<div class="indicator ${key}"></div><div class="sw-group">${group}</div>`
+        // bind click
+        const self = this
+        for (const swElment of this.root.querySelector('.sw-group').children) {
+            swElment.onclick = function() {
+                const swKey = swElment.dataset.swKey
+                const record = self.data.find(v => v.key === swKey)
+                self.render(swKey)
+                self.onSelect(swKey, record)
+            }
+        }
+    }
+}
 
 ; (async function () {
     'use strict';
@@ -126,34 +208,57 @@ class Tabs {
     }
 
     // append container for xgplayer
-    originContainer.innerHTML += `<div id="xgplayer"></div><div class="tabs-root"></div>`
+    originContainer.innerHTML += `
+    <div id="xgplayer"></div>
+    <div class="player-sider">
+    <div class="switch-root"></div>
+    <p class="ep-tip">选集：</p>
+    <div class="tabs-root"></div>
+    </div>
+    `
     // get video resource from page data
     const res = JSON.parse($$('.wp-playlist-script').textContent)
     const resPromise = res.tracks
-        .map((track, idx) => ({ key: `${idx + 1}`, label: track.caption, url: track.src1 }))
-        .map(parseResUrl)
+        .map((track, idx) => ({ ...track, key: `${idx + 1}`, label: track.caption }))
+        .map(d => parseResUrl(window.localStorage['region'], d))
     const resGroups = await Promise.all(resPromise)
 
     // init xgplayer
+    const initVolume = window.localStorage['volume'] ? parseFloat(window.localStorage['volume']) : 1
     const isWatched = window.localStorage[location.pathname]
     const initEp = isWatched ? JSON.parse(isWatched).ep : '1'
     const initPlayUrl = resGroups.find(v => v.key === initEp).url
 
+    console.warn(`当前播放资源url：${initPlayUrl}`)
     const player = new window.Player({
         id: 'xgplayer',
         url: initPlayUrl,
+        volume: initVolume,
         fluid: true,
         videoInit: true,
         lastPlayTimeHideDelay: 3,
         ...isWatched && {lastPlayTime: JSON.parse(isWatched).seek},
     })
 
+    // init switch
+    const switchs = new Switch({
+        root: $$('.switch-root'),
+        data: [{ key: 'domestic', label: '国内线路' }, { key: 'overseas', label: '海外线路' }],
+        onSwitch: (key, record) => {
+            console.warn(`切换线路：${record.label}，即将刷新页面`)
+            window.localStorage['region'] = key
+            window.location.reload()
+        }
+    })
+    switchs.render(window.localStorage['region'])
     // init tabs
     const tabs = new Tabs({
         root: $$('.tabs-root'),
         data: resGroups,
         onSelect: (key, record) => {
+            console.warn(`切换选集：【${key}】${record.label}`)
             player.src = record.url
+            console.warn(`当前播放资源url：${record.url}`)
             player.play()
         }
     })
@@ -166,6 +271,10 @@ class Tabs {
             seek: currentTime,
             ep: tabs.selectedKey,
         })
+    })
+    // update volume
+    player.on('volumechange', function({ volume }) {
+        window.localStorage['volume'] = volume
     })
 
 })()
